6e242396b11e0ae1fd18d6e3870f895cbef1eb4b
Leander Gmeiner <leander.gmeiner@gmail.com>
2025-06-04 18:10:31 +0200

    Fixed setup and made main.py runnable

--- src/services/nameserver/service.py
+++ src/services/nameserver/service.py
@@ -70,8 +70,12 @@ class NameServiceServicer(nameserver_pb2_grpc.NameServiceServicer):
                     status_pb2.Status(code=code_pb2.INVALID_ARGUMENT, message=msg)
                 )
             )
-            
-        logger.info("The service worker %s with the type %s has been registered.", str(ip) + str(port), name)
+
+        logger.info(
+            "The service worker %s with the type %s has been registered.",
+            str(ip) + str(port),
+            name,
+        )
 
         logger.info(
             "The service worker %s with the type %s has been registered.",
