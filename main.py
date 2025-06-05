0daabcaa9dcd3a8d7114af6829793bb1d238b097
Leander Gmeiner <leander.gmeiner@gmail.com>
2025-06-06 01:09:28 +0200

    Added command

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ sys.path.append("src/common/rpc")  # Needed because of protobuf
 
 
 import logging
+
 import fire
 
-
-from src.services import dispatcher, nameserver, worker
+from src.services import client, dispatcher, nameserver, worker
 
 
 def main():
@@ -17,6 +17,7 @@ def main():
             "dispatcher": dispatcher.create_server,
             "nameserver": nameserver.create_server,
             "worker": worker.create_server,
+            "exec": client.execute_command,
         }
     )
 
