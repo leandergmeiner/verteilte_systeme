ddf72978f9529242ff6e35663b32c7d1c8fa19ea
Leander Gmeiner <leander.gmeiner@gmail.com>
2025-06-05 13:01:11 +0200

    Added worker

--- src/common/rpc/worker_pb2.py
+++ src/common/rpc/worker_pb2.py
@@ -4,15 +4,18 @@
 # source: worker.proto
 # Protobuf Python Version: 5.29.0
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 5, 29, 0, "", "worker.proto"
+    _runtime_version.Domain.PUBLIC,
+    5,
+    29,
+    0,
+    '',
+    'worker.proto'
 )
 # @@protoc_insertion_point(imports)
 
@@ -27,7 +30,7 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0cworker.proto\x
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "worker_pb2", _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'worker_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_WORKERSTATUS']._serialized_start=69
