0e93e8618039845668918e027d5cca625b52ccaa
Leander Gmeiner <leander.gmeiner@gmail.com>
2025-06-06 00:59:09 +0200

    Adjusted protocols, wrote the dispatcher and added logging

--- src/services/nameserver/service.py
+++ src/services/nameserver/service.py
@@ -70,6 +70,8 @@ class NameServiceServicer(nameserver_pb2_grpc.NameServiceServicer):
                     status_pb2.Status(code=code_pb2.INVALID_ARGUMENT, message=msg)
                 )
             )
+            
+        logger.info("The service worker %s with the type %s has been registered.", str(ip) + str(port), name)
 
         logger.info(
             "The service worker %s with the type %s has been registered.",
